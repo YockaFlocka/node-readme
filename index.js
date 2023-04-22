4d6d0dd0d40fa797450d1e1d484a712371df959b
Peter Yockey <peterjyockey@gmail.com>
2023-04-22 16:09:45 -0500

    completed writeToFile function

--- index.js
+++ index.js
@@ -125,7 +125,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        };
+      console.log("Success! You can preview your README.md file");  
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {}
