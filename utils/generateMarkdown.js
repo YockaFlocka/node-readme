0d0dd83da7d8969f859be64d5f4684221c110cf5
Peter Yockey <peterjyockey@gmail.com>
2023-04-23 12:08:03 -0500

    added readme layout in generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,6 +13,41 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  
-  `;
+
+## Description
+
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contribution}
+
+## Testing:
+
+${data.tests}
+
+## Questions
+
+If you have any questions or concerns you can reach me through GitHub or Email down below.
+
+- Github: [${data.github}](https://github.com/${data.github})
+- Email: [${data.email}](mailto:${data.email}) 
+`;
 }
+
+module.exports = generateMarkdown;
